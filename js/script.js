847ac40b843c38e6b78a3e88c017642541b12540
IvanTsve <iv_plamenovv@abv.bg>
2020-05-07 13:02:43 +0300

    added project section

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $(".popup[rel^='group']").fancybox({
-        'cycling': true,
-        'transitionin': 'elastic',
-        'transitionout': 'elastic',
-        'overlayColor': '#ccc',
-    })
-})
